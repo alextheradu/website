01ce28f22bda8dccf69f0e24e276982f0410a315
Alex Radu <cooldudev.super@gmail.com>
2025-02-05 13:07:37 -0500

    Add initial HTML structure and styles for Serenities Online

---
+++ serenities/serenity-api/public/index.js
@@ -0,0 +1,3 @@
+function button1() {
+        window.open("https://www.google.com/search?sca_esv=2f83c0f5b204b677&sxsrf=AHTn8zp_rX1m_fZj54fx9TJzpAMXI7Z6pg:1737672859539&q=serenity+person&udm=2&fbs=ABzOT_CWdhQLP1FcmU5B0fn3xuWpA-dk4wpBWOGsoR7DG5zJBtmuEdhfywyzhendkLDnhcq4Fx59GJo42IHin57PCNq1_iXUPjUX3tz6PBVMzkZC-TM1H05WTFU0FLLEd0TspVgXYhiotaHfNQfKIMh2D9jLpgvtWfgTQ29mVsROwilMq52PZ7A5qJPz0YEz1IH0VMBp3UGXe8SUCgj7DLEjSm4omkXASw&sa=X&ved=2ahUKEwjL_5zs94yLAxWKKlkFHdu9OoIQtKgLegQIDxAB&biw=1440&bih=664&dpr=2", "_blank");
+      }
